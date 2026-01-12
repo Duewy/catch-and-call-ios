5ea73725654b110dbd66f18a5faacbed38c40969
Dwayne Brame <dwayne_brame@yahoo.ca>
2026-01-12 10:49:57 -0500

    Initial Commit

---
+++ CatchAndCall/CatchAndCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchAndCallApp.swift
+//  CatchAndCall
+//
+//  Created by Dwayne Brame on 2026-01-12.
+//
+
+import SwiftUI
+
+@main
+struct CatchAndCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchAndCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatchAndCall
+//
+//  Created by Dwayne Brame on 2026-01-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
